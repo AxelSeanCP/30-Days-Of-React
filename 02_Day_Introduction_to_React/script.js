47e3ec74de20a6077b9924e7a58192c7b0c4824a
Axel Sean Cahyono Putra <axelseancp@gmail.com>
2024-06-04 21:28:33 +0700

    react day2 #1

---
+++ 02_Day_Introduction_to_React/script.js
@@ -0,0 +1,25 @@
+// JSX Syntax
+const jsxElement = <h1>I am a JSX element</h1>
+const welcome = <h1>Welcome to 30 Days of React Challenge</h1>
+const data = <small>Oct 2, 2020</small>
+
+//const title = <h2>Getting Started React</h2>
+
+/*const header = (
+    <header>
+        <h1>Welcome to 30 Days Of React</h1>
+        <h2>Getting Started React</h2>
+        <h3>Javascript Library</h3>
+        <p>Axel Sean Cahyono Putra</p>
+        <small>Jun 6, 2024</small>
+    </header>
+)*/
+
+// class names
+const title = <h1 className='title'>Getting Started React</h1>
+const inputField = (
+    <div>
+        <label htmlFor='firstname'>First Name</label>
+        <input type='text' id='firstname' placeholder='First Name' />
+    </div>
+)
