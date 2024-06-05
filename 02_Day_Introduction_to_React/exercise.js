d1c3e1b57a20c00b415a3dfd478d3bb99ab35d2b
Axel Sean <104364854+AxelSeanCP@users.noreply.github.com>
2024-06-05 20:31:36 +0700

    react day2 #finish

---
+++ 02_Day_Introduction_to_React/exercise.js
@@ -0,0 +1,14 @@
+// Write a JSX element which displays your full name, country, title, gender, email, phone number. Use h1 for the name and p for the rest of the information and store it in a user variable
+const user = (
+    <main>
+        <h1>Axel Sean Cahyono Putra</h1>
+        <p>Indonesia</p>
+        <p>Student</p>
+        <p>Male</p>
+        <p>axelseancp@gmail.com</p>
+        <p>085334473368</p>
+    </main>
+)
+
+// Write a footer JSX element
+const footer = <footer></footer>
--- 02_Day_Introduction_to_React/script.js
+++ 02_Day_Introduction_to_React/script.js
@@ -1,7 +1,7 @@
 // JSX Syntax
-const jsxElement = <h1>I am a JSX element</h1>
-const welcome = <h1>Welcome to 30 Days of React Challenge</h1>
-const data = <small>Oct 2, 2020</small>
+// const jsxElement = <h1>I am a JSX element</h1>
+// const welcome = <h1>Welcome to 30 Days of React Challenge</h1>
+// const data = <small>Oct 2, 2020</small>
 
 //const title = <h2>Getting Started React</h2>
 
@@ -16,10 +16,88 @@ const data = <small>Oct 2, 2020</small>
 )*/
 
 // class names
-const title = <h1 className='title'>Getting Started React</h1>
-const inputField = (
-    <div>
-        <label htmlFor='firstname'>First Name</label>
-        <input type='text' id='firstname' placeholder='First Name' />
-    </div>
+// const title = <h1 className='title'>Getting Started React</h1>
+// const inputField = (
+//     <div>
+//         <label htmlFor='firstname'>First Name</label>
+//         <input type='text' id='firstname' placeholder='First Name' />
+//     </div>
+// )
+
+// injecting data to jsx element
+// const welcome = 'welcome to 30 Days Of React';
+// const title = 'Getting Started React';
+// const subtitle = 'Javascript Library';
+// const authorFirstName = 'Axel';
+// const authorLastName = 'Sean';
+// const date = 'Jun 6, 2024';
+
+// const header = (
+//     <header>
+//         <div className='header-wrapper'>
+//             <h1>{welcome}</h1>
+//             <h2>{title}</h2>
+//             <h3>{subtitle}</h3>
+//             <p>
+//                 Instructor: {authorFirstName} {authorLastName}
+//             </p>
+//             <small>Date: {date}</small>
+//         </div>
+//     </header>
+// )
+
+// injecting a string to jsx element
+// const welcome = 'Welcome to 30 Days Of React'
+// const title = 'Getting Started React'
+// const subtitle = 'JavaScript Library'
+// const firstName = 'Asabeneh'
+// const lastName = 'Yetayeh'
+// const date = 'Oct 2, 2020'
+
+// // JSX element, header
+// const header = (
+//   <header>
+//     <div className='header-wrapper'>
+//       <h1>{welcome}</h1>
+//       <h2>{title}</h2>
+//       <h3>{subtitle}</h3>
+//       <p>
+//         Instructor: {firstName} {lastName}
+//       </p>
+//       <small>Date: {date}</small>
+//     </div>
+//   </header>
+// )
+
+// injecting a number to a jsx element
+// const numOne = 3;
+// const numTwo = 2;
+
+// const result = (
+//     <p>
+//         {numOne} + {numTwo} = {numOne + numTwo}
+//     </p>
+// )
+
+// const yearBorn = 2003;
+// const currentYear = new Date().getFullYear();
+// const age = currentYear - yearBorn;
+// const personAge = <p> {age} </p>
+
+// injecting an array to jsx element
+const techs = ['HTML', 'CSS', 'JavaScript'];
+
+const main = (
+    <main>
+        <div className='main-wrapper'>
+            <p>
+                Prequisite to get started{' '}
+                <strong>
+                    <em>react.js</em>
+                </strong>
+                :
+            </p>
+            <ul>{techs}</ul>
+        </div>
+    </main>
 )
